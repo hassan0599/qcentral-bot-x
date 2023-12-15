966fe74549cbe36a4beed6454a9c0e454beda07c
hassan0599 <hassan0599@gmail.com>
2023-12-15 02:18:44 -0500

    Created an X bot for posting on X

---
+++ main.py
@@ -0,0 +1,90 @@
+import base64
+import hashlib
+import os
+import re
+import json
+import requests
+import redis
+from requests.auth import AuthBase, HTTPBasicAuth
+from requests_oauthlib import OAuth2Session, TokenUpdated
+from flask import Flask, request, redirect, session, url_for, render_template
+
+r = redis.from_url(os.environ["REDIS_URL"])
+
+app = Flask(__name__)
+app.secret_key = os.urandom(50)
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+auth_url = "https://twitter.com/i/oauth2/authorize"
+token_url = "https://api.twitter.com/2/oauth2/token"
+redirect_uri = os.environ.get("REDIRECT_URI")
+
+scopes = ["tweet.read", "users.read", "tweet.write", "offline.access"]
+
+code_verifier = base64.urlsafe_b64encode(os.urandom(30)).decode("utf-8")
+code_verifier = re.sub("[^a-zA-Z0-9]+", "", code_verifier)
+
+code_challenge = hashlib.sha256(code_verifier.encode("utf-8")).digest()
+code_challenge = base64.urlsafe_b64encode(code_challenge).decode("utf-8")
+code_challenge = code_challenge.replace("=", "")
+
+
+def make_token():
+    return OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scopes)
+
+
+def parse_quote():
+    url = "https://api.quotable.io/quotes/random"
+    params = {'maxLength': 260}
+    response_raw = requests.request("GET", url, params=params).json()
+    response = []
+    response.append(response_raw[0]["content"])
+    response.append(response_raw[0]["author"])
+    return response
+
+
+def post_tweet(payload, token):
+    print("Tweeting!")
+    return requests.request(
+        "POST",
+        "https://api.twitter.com/2/tweets",
+        json=payload,
+        headers={
+            "Authorization": "Bearer {}".format(token["access_token"]),
+            "Content-Type": "application/json",
+        },
+    )
+
+
+@app.route("/")
+def demo():
+    global x
+    x = make_token()
+    authorization_url, state = x.authorization_url(
+        auth_url, code_challenge=code_challenge, code_challenge_method="S256"
+    )
+    session["oauth_state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/oauth/callback", methods=["GET"])
+def callback():
+    code = request.args.get("code")
+    token = x.fetch_token(
+        token_url=token_url,
+        client_secret=client_secret,
+        code_verifier=code_verifier,
+        code=code,
+    )
+    st_token = '"{}"'.format(token)
+    j_token = json.loads(st_token)
+    r.set("token", j_token)
+    quote = parse_quote()
+    payload = {"text": "{}\n- {}".format(quote[0], quote[1])}
+    response = post_tweet(payload, token).json()
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
